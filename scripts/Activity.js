dffda9d41d63a73f990583cd6e01f627d0f97eb5
mbcorvalan <mariacorvalanamil@gmail.com>
2024-03-16 01:13:13 -0300

    fixed changes

--- scripts/Activity.js
+++
@@ -1,8 +0,0 @@
-export class Activity {
-    constructor(id, title, description, imgUrl) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.imgUrl = imgUrl;
-    }
-}
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,37 @@
-import { Repository } from './Repository.js';
-
 const favoriteActivitiesForm = document.querySelector('#favorite-activities-form');
 const activitiesContainer = document.querySelector('#activitiesContainer');
 const activityTitle = document.querySelector('#activityTitle');
 const activityDescription = document.querySelector('#activityDescription');
 const activityImageUrl = document.querySelector('#activityImageUrl');
+class Activity {
+    constructor(id, title, description, imgUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+}
+
+class Repository {
+    constructor() {
+        this.activities = [];
+        this.nextId = 0;
+    }
+
+    getAllActivities() {
+        return this.activities;
+    }
+
+    createActivity(title, description, imgUrl) {
+        const newActivity = new Activity(this.nextId++, title, description, imgUrl);
+        this.activities.push({ ...newActivity });
+    }
+
+    deleteActivity(id) {
+        this.activities = this.activities.filter(activity => activity.id !== id);
+    }
+}
+
 
 const repository = new Repository();
 
